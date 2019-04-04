29b3108d13e3d7c72cc55c049bd71d19139fd8c7
Kaldain <45125033+Kaldain@users.noreply.github.com>
2019-04-04 14:55:11 +0200

    Attaking 1.1
    atak ładny ale dmg nie zadasz

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -6,89 +6,114 @@ var GRAVITY = 10
 var JUMP = -300
 var FLOOR = Vector2(0, -1)
 
-var timer
 var attack_delay = 1
-var attack = false
+var attacking = false
+var attack_anim = null
+var anim_numb = 1
 
 var velocity = Vector2()
 
-func timing():
-	if Timer.set_timer_falue(1):
-		attack = false
 
 func get_input():
-	
-	#atak
-	if Input.is_action_just_pressed("action_attack"):
-		attack = true
-	elif attack == true:
-		$Sprite.play("Attack")
-		timer = Timer.new()
-		timer.set_one_shot(true)
-		timer.set_wait_time(attack_delay)
-		timer.connect("timeout",self,"on_timeout_complete")
-		add_child(timer)
-		timer.start()
-		velocity.x = 0
-		
-	#inventarz
-	if Input.is_action_pressed("action_inventory"):
-		var invenotry = load("inventory.tscn")
-		var node = invenotry.instance()
-		add_child(node)
-#	elif Input.is_action_pressed("action_inventory") && inv == true:
-#		inv = false
-#		remove_child(node)
-		
-	#ruch w prawo
-	if Input.is_action_pressed("move_right"):
-		velocity.x = SPEED
-		$Sprite.flip_h = false
-		$Sprite.play("Run")
-		
-	#ruch w lewo
-	elif Input.is_action_pressed("move_left"):
-		velocity.x = -SPEED
-		$Sprite.flip_h = true
-		$Sprite.play("Run")
-		
-	#bezruch
-	else: 
-		velocity.x = 0 
-		$Sprite.play("Idle-NoSword")
-		
-	#skok
-	if is_on_floor():
-		if attack ==false:
+	var friction = false
+	attack_anim = "Attack" +str(anim_numb)
+	print(anim_numb)
+
+
+	if attacking == false:
+			
+		#ruch w prawo
+		if Input.is_action_pressed("move_right"):
+			velocity.x = SPEED
+			$Sprite.flip_h = false
+			$Sprite.play("Run")
+			
+		#ruch w lewo
+		elif Input.is_action_pressed("move_left"):
+			velocity.x = -SPEED
+			$Sprite.flip_h = true
+			$Sprite.play("Run")
+			
+		else:
+			friction = true
+			
+		if is_on_floor():
+			if friction == true:
+				velocity.x = 0
+				if $Timer2.time_left > 0:
+					$Sprite.play("Idle-Sword")
+				else:
+					$Sprite.play("Idle-NoSword")
+				
+		#bezruch
+#		else: 
+#			velocity.x = 0 
+#			$Sprite.play("Idle-NoSword")
+			
+			
+		#ślizg
+		if Input.is_action_pressed("action_slide"):
+			if $Sprite.flip_h == true:
+				self.velocity -= Vector2(150, 0)
+				$Sprite.play("Slide")
+			elif $Sprite.flip_h == false:
+				self.velocity += Vector2(150, 0)
+				$Sprite.play("Slide")
+			
+		#skok
+		if is_on_floor():
 			if Input.is_action_just_pressed("action_jump"):
 				velocity.y = JUMP
-	else:
-		$Sprite.play("Jump")
-		
-	#ślizg
-	if Input.is_action_pressed("action_slide"):
-		if $Sprite.flip_h == true:
-			velocity.x = -SSPEED
-			$Sprite.play("Slide")
-		elif $Sprite.flip_h == false:
-			velocity.x = SSPEED
-			$Sprite.play("Slide")
+		else:
+			$Sprite.play("Jump")
+			
+			#atak
+		if Input.is_action_just_pressed("action_attack"):
+			$Timer.start()
+			$Timer2.start()
+			attack()
+			
+			if $Timer.time_left > 0:
+				anim_numb += 1
+			
+			if anim_numb == 4:
+				anim_numb = 1
+
+
+
 
 	velocity.y += GRAVITY
 	
 	#ESC wywala gre i elo
 func _unhandled_input(event):
-    if event is InputEventKey:
-        if event.pressed and event.scancode == KEY_ESCAPE:
-            get_tree().quit()
+	if event is InputEventKey:
+		if event.pressed and event.scancode == KEY_ESCAPE:
+			get_tree().quit()
 
 
-func on_timeout_complete():
-	attack  = false
+func attack():
+	attacking = true
+	velocity.x = 0
+	if $Sprite.flip_h == false:
+		self.position += Vector2(20, 0)
+	else:
+		self.position -= Vector2(20, 0)
+	$Sprite.play(attack_anim)
+	yield($Sprite, "animation_finished")
+	attacking = false
+
 
 func _physics_process(delta):
 	
 	get_input()
-
+	
 	
 	velocity = move_and_slide(velocity,FLOOR)
+
+
+func _on_Timer_timeout():
+	anim_numb = 1
+
+
+func _on_Timer2_timeout():
+	pass # replace with function body
