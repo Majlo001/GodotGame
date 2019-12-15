fbe648e95536b68c25b5007d0b47bb8ccc9e39f7
Majlo001 <45098860+Majlo001@users.noreply.github.com>
2019-12-15 17:55:01 +0100

    1.7.5

--- lever.gd
+++ lever.gd
@@ -14,7 +14,8 @@ func _input(_event):
 	if Input.is_action_just_pressed("ui_e") and interact == true:
 		var lever_off = get_parent().get_node("lever/lever_off")
 		var lever_on = get_parent().get_node("lever/lever_on")
+		var wall = get_parent().get_node("wall")
 		lever_off.hide()
 		lever_on.show()
-
+		wall.position = Vector2(0,-140)
 
