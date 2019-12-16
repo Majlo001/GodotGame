f7518eac4cdf05e4be8ffe53f6a314067f582bf0
Pr0r0k <kacperdlugopolski@gmail.com>
2019-12-16 19:08:45 +0100

    1.7.5

--- Zombie_Summon.gd
+++ Zombie_Summon.gd
@@ -26,7 +26,7 @@ var next_dir_time = 0
 
 var next_jump_time = -1
 
-var target_player_dsit = 100
+var target_player_dsit = 70
 
 var eye_reach = 15
 var vision = 200
@@ -93,9 +93,10 @@ func _physics_process(_delta):
 		elif Player.position.x > position.x + target_player_dsit and sees_player():
 			$Sprite.play("Walk")
 			set_dir(1)
-		elif Player.position.x < position.x - target_player_dsit or Player.position.x > position.x + target_player_dsit and sees_player():
+		elif Player.position.x > position.x - target_player_dsit or Player.position.x < position.x + target_player_dsit and sees_player():
 			print("spelnia")
-			$Timer.start()
+#			$Timer.start()
+			attack()
 		else:
 			$Sprite.play("Idle")
 			set_dir(0)
@@ -168,7 +169,7 @@ func attack():
 		$Sprite.play("Attack")
 		weapon.attack()
 		yield($Sprite, "animation_finished")
-		$Timer.start()
+#		$Timer.start()
 		attacking = false
 
 func take_damage(count):
