23876925a8564b1849608eec97b4116370f66407
Majlo <szmilosz01@gmail.com>
2020-05-25 16:26:23 +0200

    0.7.7.5
    Skills Panel Part 2

--- Scenes/Skills.gd
+++ Scenes/Skills.gd
@@ -12,7 +12,7 @@ onready var Janek = get_parent().get_parent().get_node('Janek')
 
 onready var frame : Node = $Panel
 
-onready var strength_count : Node = $Panel/HBoxContainer/VBoxContainer/Charisma/LabelCount
+onready var strength_count : Node = $Panel/HBoxContainer/VBoxContainer/Strength/LabelCount
 onready var dexterity_count : Node = $Panel/HBoxContainer/VBoxContainer/Dexterity/LabelCount
 onready var durability_count : Node = $Panel/HBoxContainer/VBoxContainer/Durability/LabelCount
 onready var inteligence_count : Node = $Panel/HBoxContainer/VBoxContainer/Inteligence/LabelCount
@@ -35,13 +35,24 @@ onready var points_count : Node = $Panel/HBoxContainer/VBoxContainer/Points/Labe
 var visible_panel = false
 
 var points = 3
+var strength
+var dexterity
+var durability
+var inteligence
+var charisma
 
 func _ready():
-	strength_count.text = str(Janek.strength)
-	dexterity_count.text = str(Janek.dexterity)
-	durability_count.text = str(Janek.durability)
-	inteligence_count.text = str(Janek.inteligence)
-	charisma_count.text = str(Janek.charisma)
+	strength = str(Janek.strength)
+	dexterity = str(Janek.dexterity)
+	durability = str(Janek.durability)
+	inteligence = str(Janek.inteligence)
+	charisma = str(Janek.charisma)
+
+	strength_count.text = strength
+	dexterity_count.text = dexterity
+	durability_count.text = durability
+	inteligence_count.text = inteligence
+	charisma_count.text = charisma
 	points_count.text = str(points)
 	
 	strength_minus.disabled = true
@@ -60,3 +71,46 @@ func _input(event):
 			Janek.can_move = true
 			visible_panel = false
 			frame.hide()
+
+
+
+func _on_ButtonPlus_pressed(extra_arg):
+	print(extra_arg)
+	
+	if extra_arg == "str" and points > 0:
+		strength_minus.disabled = false
+		points-=1
+		strength = int(strength)+1 
+		refresh()
+		
+	if extra_arg == "dex" and points > 0:
+		dexterity_minus.disabled = false
+		points-=1
+		dexterity = int(dexterity)+1 
+		refresh()
+	
+	if extra_arg == "dur" and points > 0:
+		durability_minus.disabled = false
+		points-=1
+		durability = int(durability)+1 
+		refresh()
+	
+	if extra_arg == "int" and points > 0:
+		inteligence_minus.disabled = false
+		points-=1
+		inteligence = int(inteligence)+1 
+		refresh()
+	
+	if extra_arg == "cha" and points > 0:
+		charisma_minus.disabled = false
+		points-=1
+		charisma = int(charisma)+1 
+		refresh()
+
+func refresh():
+		strength_count.text = str(strength)
+		dexterity_count.text = str(dexterity)
+		durability_count.text = str(durability)
+		inteligence_count.text = str(inteligence)
+		charisma_count.text = str(charisma)
+		points_count.text = str(points)
