a574f58eeacfc70fa0456ef1029b80beb758bb24
Majlo <szmilosz01@gmail.com>
2020-06-16 21:58:03 +0200

    0.7.8.4

--- src/Titlescene.gd
+++ src/Titlescene.gd
@@ -5,6 +5,7 @@ onready var NewGameButton = $HBoxContainer/VBoxContainer2/NewGame
 onready var LoadGameButton = $HBoxContainer/VBoxContainer2/LoadGame
 onready var OptionsButton = $HBoxContainer/VBoxContainer2/Options
 onready var MultiplayerButton = $HBoxContainer/VBoxContainer2/Multiplayer
+onready var Exit = $HBoxContainer/VBoxContainer2/Exit
 
 func _ready():
 	NewGameButton.grab_focus()
@@ -18,3 +19,25 @@ func _physics_process(_delta):
 		OptionsButton.grab_focus()
 	if MultiplayerButton.is_hovered() == true:
 		MultiplayerButton.grab_focus()
+	if Exit.is_hovered() == true:
+		Exit.grab_focus()
+
+
+func _on_NewGame_pressed():
+	pass # Replace with function body.
+
+
+func _on_LoadGame_pressed():
+	pass # Replace with function body.
+
+
+func _on_Options_pressed():
+	pass # Replace with function body.
+
+
+func _on_Multiplayer_pressed():
+	pass # Replace with function body.
+
+
+func _on_Exit_pressed():
+	pass # Replace with function body.
