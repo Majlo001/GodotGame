9cd51ff2da97443c7f3250625630569869a8db45
Majlo <szmilosz01@gmail.com>
2020-08-07 15:27:10 +0200

    0.7.9.3

--- src/Quest/Quest.gd
+++ src/Quest/Quest.gd
@@ -1,14 +1,18 @@
 extends Node
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var quest_folder = "res://src/Quest/quests"
 
+var quest
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-func startQuest(quest_name):
-	print("!11")
+func startQuest(quest_name, id='001'):
+	var file = File.new()
+	file.open('%s/%s.json' % [quest_folder, id], file.READ) # ID dać do fora
+	var json = file.get_as_text()
+	quest = JSON.parse(json).result
+	file.close()
+	print(quest)
